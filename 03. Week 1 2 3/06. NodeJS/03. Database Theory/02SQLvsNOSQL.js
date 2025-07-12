6f0d727815b9bbff3335293ca57526b095bd50f2
dev-arshdeepsingh <167911463+dev-arshdeepsingh@users.noreply.github.com>
2025-07-12 14:58:16 +0530

    Add files via upload

---
+++ 03. Week 1 2 3/06. NodeJS/03. Database Theory/02SQLvsNOSQL.js
@@ -0,0 +1,22 @@
+// SQL is a domain specific lang. it only have simple english like queries. 
+// SQl is used to create & modify DB's schema which is nothing but the structure of the DB.
+// Structured data is any data that looks like a table or a spreadsheet like excel. 
+// ie we know precisely where name will go where last name will go where email will go.
+// structure remains same throughout the data for every user.
+// If we want to add a new column, we need to modify the whole schema for every user.
+// SQL follows this hard structured data.
+// SQL is also called as Relational Database Management System (RDBMS) which is data in the form of rows & columns.
+
+//Relation b/w 2 or more tables is formed with a key called as Foreign Key.
+// Each table has a primary key which is unique for each row. 
+// Hence foreig key of one table can be primary key of another table.
+
+// NoSQL is a non-relational database management system (NRDBMS) that stores data in a non-tabular format.
+// This can be as flexible as we want & as rigid as SQL. So yeah NOSQL can behave as SQL if we add to much of the restrictions.
+// But their structure still will be diff. as SQL will be tabular form while NOSQL will be in document form.
+
+//In NOSQL, we can store one person's data with Only "Name", while the other person's data can have "Name", "Email", "Phone" etc. & both can be stored together in same DB. But it's not possible in SQL as there is a schema.
+// SQL is very rigid in terms of structure.
+// in NOSQL, we can defne new structure for some of the data like Mp3 files etc.
+// Types of NOSQL: document, key-value, graph, columnar etc.
+// NoSQL are known for scaling horizontally. 
